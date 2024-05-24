c08356530790b11960e3597741b2555ab93924cd
Daniil Astapenko <astapenko.daniil.03@gmail.com>
2024-05-24 12:34:03 +0200

    Add EntityGraph in Flight repository to findAll (pagination method)

--- airline_reservation_system/src/main/java/cz/mendelu/pef/airline_reservation_system/domain/flight/FlightRepository.java
+++ airline_reservation_system/src/main/java/cz/mendelu/pef/airline_reservation_system/domain/flight/FlightRepository.java
@@ -1,5 +1,8 @@
 package cz.mendelu.pef.airline_reservation_system.domain.flight;
 
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -8,9 +11,16 @@ import java.time.OffsetDateTime;
 import java.util.List;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+    @Override
+    @EntityGraph(attributePaths = {"tickets"})
+    @NonNull
+    Page<Flight> findAll(@NonNull Pageable pageable);
+
     Iterable<Flight> getFlightsByFareTariff_IdEquals(Long id);
 
     @EntityGraph(attributePaths = {"tickets"})
+    @NonNull
     List<Flight> getFlightsByDepartureGreaterThanEqualAndArrivalLessThan(OffsetDateTime startDate, OffsetDateTime endDate);
 
     @Query("""
